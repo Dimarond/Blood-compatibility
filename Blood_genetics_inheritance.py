05bf73767be2508185bee3be8b2fbe5bac343469
Craciun Dumitru M.D <59809057+Dimarond@users.noreply.github.com>
2023-05-03 13:55:20 +0300

    Update Blood_genetics_inheritance.py

--- Blood_genetics_inheritance.py
+++ Blood_genetics_inheritance.py
@@ -1,5 +1,4 @@
 alleles = ['AO', 'AA', 'BO', 'BB', 'OO', 'AB']
 
-parent1 = input("What blood type does the parent have?: ")
-
-
+parent1 = input("What blood type does parent1 have?: ")
+parent2 = input("What blood type does parent2 have?: ")
