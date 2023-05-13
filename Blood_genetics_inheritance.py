4269f665aa3f77b01fd570d9cae9e85e746a65fe
Craciun Dumitru M.D <59809057+Dimarond@users.noreply.github.com>
2023-05-13 20:47:54 +0300

    Update Blood_genetics_inheritance.py

--- Blood_genetics_inheritance.py
+++ Blood_genetics_inheritance.py
@@ -1,4 +1,59 @@
-alleles = ['AO', 'AA', 'BO', 'BB', 'OO', 'AB']
+def calculate_child_blood_types(parent1, parent2):
 
-parent1 = input("What blood type does parent1 have?: ")
-parent2 = input("What blood type does parent2 have?: ")
+	alleles = {
+		'A': ['A', 'O'],
+		'B': ['B', 'O'],
+		'AB': ['A', 'B'],
+		'O': ['O']
+	}
+
+	parent1_alleles = alleles[parent1[0]] + alleles[parent1[1]]
+	parent2_alleles = alleles[parent2[0]] + alleles[parent2[1]]
+
+	if parent1[1] != 'O':
+		parent1_alleles = parent1_alleles[:-1]
+	if parent2[1] != 'O':
+		parent2_alleles = parent2_alleles[:-1]
+        
+	child_blood_types = {
+		'A': 0,
+		'B': 0,
+		'AB': 0,
+		'O': 0
+	}
+
+
+	for allele1 in parent1_alleles:
+		for allele2 in parent2_alleles:
+			if allele1 == 'A' and allele2 == 'B':
+				child_blood_types['AB'] += 1
+			elif allele1 == 'A' and allele2 == 'A':
+				child_blood_types['A'] += 1
+			elif allele1 == 'B' and allele2 == 'B':
+				child_blood_types['B'] += 1
+			elif allele1 == 'O' and allele2 == 'O':
+				child_blood_types['O'] += 1
+			elif allele1 == 'A' and allele2 == 'O':
+				child_blood_types['A'] += 1
+				child_blood_types['O'] += 1
+			elif allele1 == 'B' and allele2 == 'O':
+				child_blood_types['B'] += 1
+				child_blood_types['O'] += 1
+	total = sum(child_blood_types.values())
+
+	for blood_type in child_blood_types:
+		child_blood_types[blood_type] /= total
+
+	return child_blood_types
+
+
+parent1 = input("What is parent1 allele type?: ")
+while len(parent1) < 2:
+	print("Please input a valid allele corespinding to a blood type ex: AO, BO, AB or OO")
+	parent1 = input("What is parent1 blood type?: ")
+parent2 = input("What is parent2 blood type?: ")
+while len(parent2) < 2:
+	print("Please input a valid allele corespinding to a blood type ex: AO, BO, AB or OO")
+	parent2 = input("What is parent2 allele type?: ")
+child_blood_types = calculate_child_blood_types(parent1, parent2)
+print(child_blood_types)
